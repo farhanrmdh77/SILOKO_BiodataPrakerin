c05dea3aecd206679e6fa0dbdd3bba191a38476c
farhanrmdh77 <ramadhanfarhan446@gmail.com>
2026-09-13 10:24:31 +0700

    Add BPK logo favicon to all views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/logo.png') }}">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -98,3 +99,4 @@
         </div>
     </body>
 </html>
+
